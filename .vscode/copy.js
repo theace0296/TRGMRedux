8c3d7a385294e1ea8ef8ec664add1fb2cf4af0b9
TheAce0296 <29675446+theace0296@users.noreply.github.com>
2024-01-26 13:35:26 -0500

    pretty copy output

--- .vscode/copy.js
+++ .vscode/copy.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const fsp = require('fs/promises');
 const path = require('path');
-const { execSync } = require('child_process');
+const { exec } = require('child_process');
 
 const cwd = process.cwd();
 const unwantedFoldersAndFiles = ['mission.sqm', '.vscode', '.github', '.git', '.gitignore', 'Mission-Templates', 'Pbo-Tools', 'Tools'];
@@ -58,26 +58,89 @@ const copyTrgmReduxFiles = async (folder, parentFolder) => {
     })
   );
   if (makePbos) {
-    execSync(`makepbo -P -A -$ -B -X=".bak,.txt" "${folderPath}"`, { cwd: parentFolder });
+    const process = exec(`makepbo -P -A -$ -B -X=".bak,.txt" "${folderPath}"`, { cwd: parentFolder });
+    await new Promise(resolve => {
+      process.on('close', resolve);
+    })
     await ensuredCopy(path.join(parentFolder, `${folder}.pbo`), path.join(destinationDir, `${folder}.pbo`));
     await fsp.rm(path.join(parentFolder, `${folder}.pbo`), { force: true });
   }
-  console.log(`-----------------------------------------------------\nMission Folder: ${folder}\n-----------------------------------------------------`);
+  // console.log(`-----------------------------------------------------\nMission Folder: ${folder}\n-----------------------------------------------------`);
 };
 
-(async () => {
-  await Promise.all(additionalFolders.map(additionalFolder => copyTrgmReduxFiles(additionalFolder, folderParentDir)));
-  for (const secondaryFolder of secondaryFolders) {
-    await Promise.all(
-      fs.readdirSync(path.join(folderParentDir, secondaryFolder)).map(async missionFolder => {
-        const parentFolder = path.join(folderParentDir, secondaryFolder);
-        if (copyMissionFiles) {
-          await copyTrgmReduxFiles(missionFolder, parentFolder);
-        }
-        if (makeTemplates && secondaryFolder !== 'TRGM-Custom') {
-          await ensuredCopy(path.join(parentFolder, missionFolder, 'mission.sqm'), path.join(templateFolder, missionFolder, 'mission.sqm'));
-        }
-      })
-    );
+
+const progress = new class Progress {
+  constructor() {
+    if (!process.stdout.isTTY) {
+      this.width = 100;
+    } else {
+      this.width = Math.ceil((process.stdout.getWindowSize()[0] + 1) / 2);
+    }
   }
+  print(current, total, name = '') {
+    if (total <= 0) {
+      total = 1;
+    }
+    if (current >= total) {
+      current = total;
+    }
+    const percent = Math.round((current / total) * 100);
+    const completeCount = Math.round((current / total) * this.width);
+    const barCompleteChar = '\u2588';
+    const barIncompleteChar = '\u2591';
+    const bar = `${barCompleteChar.repeat(completeCount)}${barIncompleteChar.repeat(this.width - completeCount)}`;
+    let string = `| ${bar} ${percent}% | ${current}/${total} |`;
+    if (name) {
+      string = `${string} ${name} |`
+    }
+
+    if (!process.stdout.isTTY) {
+      process.stdout.write(`${string}\n`);
+    } else {
+      process.stdout.cursorTo(0, null);
+      process.stdout.write(string);
+      process.stdout.clearLine(1);
+    }
+  }
+}
+
+const operations = new Proxy({ total: 0, complete: 0, name: '' }, {
+  get(target, property) {
+    return Reflect.get(target, property);
+  },
+  set(target, property, value) {
+    const result = Reflect.set(target, property, value);
+    if (!result) {
+      return false;
+    }
+    progress.print(target.complete, target.total, target.name);
+    return true;
+  }
+});
+
+
+
+(async () => {
+  operations.total += additionalFolders.length;
+  for (const additionalFolder of additionalFolders) {
+    operations.name = additionalFolder;
+    await copyTrgmReduxFiles(additionalFolder, folderParentDir);
+    operations.complete++;
+  }
+  for (const secondaryFolder of secondaryFolders) {
+    const parentFolder = path.join(folderParentDir, secondaryFolder);
+    const missionFolders = await fsp.readdir(parentFolder);
+    operations.total += missionFolders.length;
+    for (const missionFolder of missionFolders) {
+      operations.name = missionFolder;
+      if (copyMissionFiles) {
+        await copyTrgmReduxFiles(missionFolder, parentFolder);
+      }
+      if (makeTemplates && secondaryFolder !== 'TRGM-Custom') {
+        await ensuredCopy(path.join(parentFolder, missionFolder, 'mission.sqm'), path.join(templateFolder, missionFolder, 'mission.sqm'));
+      }
+      operations.complete++;
+    }
+  }
+  process.stdout.write('\nDONE\n\n');
 })();
